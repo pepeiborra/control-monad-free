ade081b4a28c529b448253a5b3b0bd0375efcc05
Pepe Iborra <pepeiborra@gmail.com>
2009-05-15 19:16:13 +0200

    Adjust the semantics of free Zip
    I think this version is more useful.
    I didn't manage to implement unification with the previous
    one, but this one works easily.

--- Control/Monad/Free/Zip.hs
+++ Control/Monad/Free/Zip.hs
@@ -1,21 +1,21 @@
 module Control.Monad.Free.Zip (zipFree, zipFree_) where
 
 import Control.Monad.Free
-import Control.Monad.State
+import Control.Monad.Trans
+import Control.Monad.Trans.State
 import Data.Foldable
 import Data.Traversable as T
 
-zipFree :: (Traversable f, Eq (f ()), Monad m) => (a -> b -> m c) -> Free f a -> Free f b -> m (Free f c)
-zipFree f (Pure a) (Pure b) = Pure `liftM` f a b
+zipFree :: (Traversable f, Eq (f ()), Monad m) => (Free f a -> Free f b -> m (Free f c))
+                                                -> Free f a -> Free f b -> m (Free f c)
 zipFree f m1@(Impure a) m2@(Impure b)
-      | fmap (const ()) a == fmap (const ()) b = Impure `liftM` unsafeZipWithG (zipFree f) a b
-zipFree _ _ _ = fail "zipFree: not the same structure"
+      | fmap (const ()) a == fmap (const ()) b = Impure `liftM` unsafeZipWithG f a b
+zipFree _ _ _ = fail "zipFree: structure mistmatch"
 
-zipFree_ :: (Traversable f, Eq (f ()), Monad m) => (a -> b -> m ()) -> Free f a -> Free f b -> m ()
-zipFree_ f (Pure a) (Pure b) = f a b
+zipFree_ :: (Traversable f, Eq (f ()), Monad m) => (Free f a -> Free f b -> m ()) -> Free f a -> Free f b -> m ()
 zipFree_ f m1@(Impure a) m2@(Impure b)
-      | fmap (const ()) a == fmap (const ()) b = zipWithM_ (zipFree_ f) (toList a) (toList b)
-zipFree_ _ _ _ = fail "zipFree: not the same structure"
+      | fmap (const ()) a == fmap (const ()) b = zipWithM_ f (toList a) (toList b)
+zipFree_ _ _ _ = fail "zipFree_: structure mismatch"
 
 
 unsafeZipWithG :: (Traversable t1, Traversable t2, Monad m) => (a -> b -> m c) ->  t1 a -> t2 b -> m(t2 c)
